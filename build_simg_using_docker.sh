4d747445186f8f052c3c3a70bed075c9a33d87b8
Jeremy Magland <jeremy.magland@gmail.com>
2018-11-29 09:41:53 -0500

    update

--- build_simg_using_docker.sh
+++ build_simg_using_docker.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 docker run --privileged -v $PWD:/working magland/singularity:2.6.0 \
+  --user $(id -u):$(id -g) \
   bash -c "cd /working && singularity build sf_spyking_circus.simg Singularity.v0.1.0"
